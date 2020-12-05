1f996111abc8f1394c14cb10a4148c6bf3d6006d
OMITR <evgeniyygalaev@rambler.ru>
2020-12-05 15:46:10 +0300

    Init

---
+++ Data/Models/ResourceType.cs
@@ -0,0 +1,9 @@
+﻿namespace University.Data.Models
+{
+    public enum ResourceType
+    {
+        Video = 1,
+        Presentation = 2,
+        Document = 3
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using University.Data;
+using University.Data.Models;
+
+namespace University
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new UniversityContext())
+            {
+                var student = new Student();
+                student.Name = Console.ReadLine();
+                student.PhoneNumber = Console.ReadLine();
+                student.RegisteredOn = DateTime.Now;
+
+                context.Students.Add(student);
+
+                //context.Courses.Add(new Course
+                //{
+                    //Name = "Kotlin",
+                    //Price = 0,
+                    //Description = "Андроид курсы",
+                    //StartDate = DateTime.Today,
+                    //EndDate = DateTime.Today.AddDays(30)
+                //});
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
